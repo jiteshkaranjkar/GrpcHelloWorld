3bd50a541caf36a07e2ee5befafc49f47cc40943
Jitesh Karanjkar <Jitesh.Karanjkar@Nintex.com>
2023-09-07 19:56:48 +1000

    added gRPC initial tryout projects

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7019");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
